5411f1bd4a351ffb7a0cc403b060192a53030a20
Aqeeb Tasadaq <56736111+Aqeeb-Tasadaq@users.noreply.github.com>
2024-02-09 19:55:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ const __dirname = dirname(__filename);
 const app = express()
 app.use(cors(
     {
-        origin:["https://blog-site-by-aqeeb.surge.sh/"],
-        methods: ["POST", "GET"],
-        credentials: true
+        origin:["*"]
     }
 ))
 app.use(bodyParser.json({ extended: true }))
