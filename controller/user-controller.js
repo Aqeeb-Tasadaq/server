de4905b8d01fc72ca14a9c854b692ddfa2351de2
Aqeeb-Tasadaq <ch.aqeebtasadaq786@gmail.com>
2024-02-12 00:02:28 +0500

    updated

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -28,14 +28,16 @@ export const Login = async (request, response) => {
   let userMatch = await User.findOne({ email: request.body.email });
 
   if (!userMatch) {
-    return response.status(400).json({ msg: "User email not found" });
+    return response.status(200).json({ msg: "User email not found" });
   }
 
   try {
     if (request.body.password == userMatch.password) {
-      return response.status(200).json({ userData: userMatch });
+      return response.status(200).json(
+        { msg:"User Sign Up Successfully",
+          userData: userMatch });
     } else {
-      response.status(400).json({ msg: "Password not matched" });
+      response.status(200).json({ msg: "Password not matched" });
     }
   } catch (error) {
     response.status(500).json(error);
